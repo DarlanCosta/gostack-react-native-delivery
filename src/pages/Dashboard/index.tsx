1d3b5878fb1b1029c417fd3c1d212ba6d4a50116
darlancosta <darlandsv@gmail.com>
2021-03-14 12:27:49 -0300

    Backend Happy

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { Image, ScrollView } from 'react-native';
 
 import Icon from 'react-native-vector-icons/Feather';
@@ -35,6 +35,7 @@ interface Food {
   price: number;
   thumbnail_url: string;
   formattedPrice: string;
+  category: number;
 }
 
 interface Category {
@@ -54,27 +55,62 @@ const Dashboard: React.FC = () => {
   const navigation = useNavigation();
 
   async function handleNavigate(id: number): Promise<void> {
-    // Navigate do ProductDetails page
+    navigation.navigate('FoodDetails', { id });
   }
 
   useEffect(() => {
     async function loadFoods(): Promise<void> {
-      // Load Foods from API
-    }
+      const response = await api.get('foods', {
+        params: {
+          category_like: selectedCategory,
+          name_like: searchValue,
+        },
+      });
 
+      setFoods(
+        response.data.map((food: Food) => ({
+          ...food,
+          formattedPrice: formatValue(food.price),
+        })),
+      );
+    }
     loadFoods();
   }, [selectedCategory, searchValue]);
 
   useEffect(() => {
     async function loadCategories(): Promise<void> {
-      // Load categories from API
-    }
+      const response = await api.get('categories');
 
+      setCategories(response.data);
+    }
     loadCategories();
   }, []);
 
+  useEffect(() => {
+    if (searchValue.length > 0) {
+      const foodsFiteredBySearch = foods.filter(
+        food =>
+          food.description.toUpperCase().indexOf(searchValue.toUpperCase()) >
+          -1,
+      );
+      setFoods(foodsFiteredBySearch);
+    }
+
+    if (selectedCategory !== undefined) {
+      const foodsFiteredByCategory = foods.filter(
+        food => food.category === selectedCategory,
+      );
+
+      setFoods(foodsFiteredByCategory);
+    }
+  }, [searchValue, selectedCategory]);
+
   function handleSelectCategory(id: number): void {
-    // Select / deselect category
+    if (selectedCategory === id) {
+      setSelectedCategory(undefined);
+    } else {
+      setSelectedCategory(id);
+    }
   }
 
   return (
